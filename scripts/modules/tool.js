2a42fb857837cf6206659fe2dd500e7501933578
eladjmc <eladjmc88@gmail.com>
2023-02-04 16:33:36 +0200

    added cursors

--- scripts/modules/tool.js
+++ scripts/modules/tool.js
@@ -10,11 +10,14 @@ export class Tool {
     this.element.classList.add(this.type);
     if(isFirstTool){
       this.element.classList.add('tool-active');
+      document.querySelector('body').style=`cursor:url(/assets/cursors/${this.type}.cur) 20 7, auto`;
     }
     this.element.addEventListener("click", (event) => {
       onClick(this.type);
       this.element.classList.add('tool-active');
       //TODO add Mouse-pointer
+      document.querySelector('body').style=`cursor:url(/assets/cursors/${this.type}.cur) 20 7, auto`;
+      
     });
   };
 }
